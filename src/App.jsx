afecb7fe699d02ced469b291856401560f1d4297
Gopal1252 <guptagopal252@gmail.com>
2024-10-04 18:35:45 +0530

    added get connections flow

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import appStore from "./utils/appStore";
 import Profile from "./components/Profile";
 import Feed from "./components/Feed";
 import Error from "./components/Error";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
 function App() {
 
@@ -19,6 +21,8 @@ function App() {
               <Route path="/login" element={<Login/>} />
               <Route path="/profile" element={<Profile/>} />
               <Route path="/error" element={<Error/>} />
+              <Route path="/connections" element={<Connections/>} />
+              <Route path="/requests" element={<Requests/>} />
             </Route>
           </Routes>      
         </BrowserRouter>
