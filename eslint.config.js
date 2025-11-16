74319be390ba0a23fcb4154fc143f0a2b9c2015f
Gopal1252 <guptagopal252@gmail.com>
2025-11-16 09:06:01 +0530

    switched off lint errors, corrected ordering

--- eslint.config.js
+++ eslint.config.js
@@ -24,11 +24,14 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      "react/prop-types": "off",
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
+      "no-unused-vars": "off",
+      "react/prop-types": "off",
+
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
